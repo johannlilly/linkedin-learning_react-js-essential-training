cf29eea735f8e29f6e423061d8eaf2d7cd2bdfa7
Johann Lilly, Certified Agile Project Manager, Certified Blockchain Expert <johannlilly@users.noreply.github.com>
2018-01-16 16:30:34 -0500

    Refactoring elements using JSX, inline style
    nest the style object in a JSX expression

--- exercise-files/Ch02/02_02/start/dist/index.js
+++ exercise-files/Ch02/02_02/start/dist/index.js
@@ -1,19 +1,24 @@
 const { render } = ReactDOM
 
-// create a style element
-// don't forget to call it on the title element
+
 const style = {
 	backgroundColor: 'orange',
 	color: 'white',
 	fontFamily: 'verdana'
 }
 
-// style = a JSX expression (JavaScript as XML)
-// use JSX to replace the createElement call
+// using inline style instead of a style element
+// nest the style object in a JSX expression
 render(
 	<h1 id='title'
 		className='header'
-		style={style}>
+		style={
+			{
+				backgroundColor: 'orange',
+				color: 'white',
+				fontFamily: 'verdana'
+			}
+		}>
 	Hello World
 	</h1>,
 	document.getElementById('react-container')
