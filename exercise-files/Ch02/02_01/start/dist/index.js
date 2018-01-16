3a248559e2f6772c573e35ae2f5c080588b973a6
Johann Lilly, Certified Agile Project Manager, Certified Blockchain Expert <johannlilly@users.noreply.github.com>
2018-01-16 16:14:28 -0500

    Pure React, Declarative
    Describe which methods we are using as part of these passages, making our function calls shorter

--- exercise-files/Ch02/02_01/start/dist/index.js
+++ exercise-files/Ch02/02_01/start/dist/index.js
@@ -1,6 +1,11 @@
+// "declarative"
+// describe which methods we are using as part of these passages, making our function calls shorter
+const { createElement } = React
+const { render } = ReactDOM
+
 // create first react element
 // createElement takes 3 arguments (tag, element properties, child content)
-const title = React.createElement(
+const title = createElement(
 	'h1',
 	{id: 'title', className: 'header'},
 	'Hello World' 
@@ -8,7 +13,7 @@ const title = React.createElement(
 
 // render the element
 // ReactDOM takes 2 arguments: element to render, where to render in the DOM
-ReactDOM.render(
+render(
 	title,
 	document.getElementById('react-container')
 )
