2666079faa07b264ab47f838af3a49cdabea3410
Johann Lilly, Certified Agile Project Manager, Certified Blockchain Expert <johannlilly@users.noreply.github.com>
2018-02-11 16:53:33 -0500

    Adding CSS to webpack build

--- exercise-files/Ch02/02_07/start/src/lib.js
+++ exercise-files/Ch02/02_07/start/src/lib.js
@@ -1,18 +1,33 @@
 import React from 'react'
 import text from './titles.json'
+import './stylesheets/goodbye.scss'
+import './stylesheets/hello.css'
+
+// the goal is to make less HTTP requests
+// we can bundle requests for CSS
+// remove inline styles
+// use class names
+
+// add loaders to webpack.config.js to transpile SCSS
+
+// npm install autoprefixer-loader@3.2.0 --save-dev
+// npm install css-loader@0.25.0 --save-dev
+// npm install sass-loader@4.0.2 --save-dev
+// npm install node-sass@3.10.1 --save-dev
+// sudo npm install --unsafe-perm node-sass@4.7.2
+// npm install style-loader@0.13.1 --save-dev
+
 
 export const hello = (
 	<h1 id='title'
-		className='header'
-		style={{backgroundColor: 'purple', color: 'yellow'}}>
+		className='hello'>
 		{text.hello}
 	</h1>
 )
 
 export const goodbye = (
 	<h1 id='title'
-		className='header'
-		style={{backgroundColor: 'yellow', color: 'purple'}}>
+		className='goodbye'>
 		{text.goodbye}
 	</h1>
 )
--- exercise-files/Ch02/02_07/start/webpack.config.js
+++ exercise-files/Ch02/02_07/start/webpack.config.js
@@ -26,6 +26,14 @@ module.exports = {
 				test: /\.json$/,
 				exclude: /(node_modules)/,
 				loader: "json-loader"
+			},
+			{
+				test: /\.css$/,
+				loader: 'style-loader!css-loader!autoprefixer-loader'
+			},
+			{
+				test: /\.scss$/,
+				loader: 'style-loader!css-loader!autoprefixer-loader!sass-loader'
 			}
 		]
 	}
