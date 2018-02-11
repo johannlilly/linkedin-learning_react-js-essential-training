b20598c8fbba835aeed3b22b8e1d8add04eba8de
Johann Lilly, Certified Agile Project Manager, Certified Blockchain Expert <johannlilly@users.noreply.github.com>
2018-02-11 18:04:42 -0500

    Migrating to webpack 3, final

--- exercise-files/Ch02/02_08/start/upgrade-build/README.txt
+++ exercise-files/Ch02/02_08/start/upgrade-build/README.txt
@@ -5,3 +5,4 @@ npm install babel-core@6.25.0 --save-dev
 npm install babel-loader@7.1.1 --save-dev
 npm install babel-preset-env@1.6.0 --save-dev
 npm install babel-preset-react@6.24.1 --save-dev
+webpack
--- exercise-files/Ch02/02_08/start/upgrade-build/webpack.config.js
+++ exercise-files/Ch02/02_08/start/upgrade-build/webpack.config.js
@@ -3,37 +3,17 @@ var webpack = require("webpack");
 module.exports = {
 	entry: "./src/index.js",
 	output: {
-		path: "dist/assets",
-		filename: "bundle.js",
-		publicPath: "assets"
-	},
-	devServer: {
-		inline: true,
-		contentBase: './dist',
-		port: 3000
+		filename: "bundle.js"
 	},
 	module: {
-		loaders: [
+		rules: [
 			{
 				test: /\.js$/,
 				exclude: /(node_modules)/,
-				loader: ["babel-loader"],
+				loader: "babel-loader",
 				query: {
-					presets: ["latest", "stage-0", "react"]
+					presets: ["env", "react"]
 				}
-			},
-			{
-				test: /\.json$/,
-				exclude: /(node_modules)/,
-				loader: "json-loader"
-			},
-			{
-				test: /\.css$/,
-				loader: 'style-loader!css-loader!autoprefixer-loader'
-			},
-			{
-				test: /\.scss$/,
-				loader: 'style-loader!css-loader!autoprefixer-loader!sass-loader'
 			}
 		]
 	}
