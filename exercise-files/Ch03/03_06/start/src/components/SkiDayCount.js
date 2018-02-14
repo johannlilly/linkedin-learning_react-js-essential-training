e77ca6cbc235427ed1b658fadc564362355dc0a9
jlilly <johannlilly@users.noreply.github.com>
2018-02-13 21:56:05 -0500

    Creating stateless functional components, destructured

--- exercise-files/Ch03/03_06/start/src/components/SkiDayCount.js
+++ exercise-files/Ch03/03_06/start/src/components/SkiDayCount.js
@@ -29,27 +29,28 @@ const calcGoalProgress = (total, goal) => {
 
 // we pass property information to this function
 // we can also destructure it
+// instead of using the entire object, we can specify the object keys we are going to use.
 
 // export class SkiDayCount extends Component {
-export const SkiDayCount = (props) => (
+export const SkiDayCount = ({total, powder, backcountry, goal}) => (
 	<div className="ski-day-count">
 		<div className="total-days">
-			<span>{props.total}</span>
+			<span>{total}</span>
 			<span>days</span>
 		</div>
 		<div className="powder-days">
-			<span>{props.powder}</span>
+			<span>{powder}</span>
 			<span>days</span>
 		</div>
 		<div className="backcountry-days">
-			<span>{props.backcountry}</span>
+			<span>{backcountry}</span>
 			<span>days</span>
 		</div>
 		<div>
 			<span>
 				{calcGoalProgress(
-					props.total, 
-					props.goal
+					total, 
+					goal
 				)}
 			</span>
 		</div>
