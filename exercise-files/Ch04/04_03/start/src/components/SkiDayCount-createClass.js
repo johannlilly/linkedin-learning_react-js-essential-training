9314c2219cd06264f3cc1b307624f2e55b937d77
jlilly <johannlilly@users.noreply.github.com>
2018-02-14 13:07:06 -0500

    Default props, createClass

--- exercise-files/Ch04/04_03/start/src/components/SkiDayCount-createClass.js
+++ exercise-files/Ch04/04_03/start/src/components/SkiDayCount-createClass.js
@@ -5,6 +5,14 @@ import SnowFlake from 'react-icons/lib/ti/weather-snow'
 import Calendar from 'react-icons/lib/fa/calendar'
 
 export const SkiDayCount = createClass({
+  getDefaultProps() {
+    return {
+      total: 50,
+      powder: 50,
+      backcountry: 15,
+      goal: 100
+    }
+  },
   percentToDecimal(decimal) {
     return ((decimal * 100) + '%')
   },
--- exercise-files/Ch04/04_03/start/src/index.js
+++ exercise-files/Ch04/04_03/start/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { render } from 'react-dom'
 import './stylesheets/ui.scss'
-import { SkiDayCount } from './components/SkiDayCount'
+import { SkiDayCount } from './components/SkiDayCount-createClass'
 
 window.React = React
 
