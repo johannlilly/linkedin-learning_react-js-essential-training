6dbbf60dfb8fe81c73f7601635c07f50ad319321
jlilly <johannlilly@users.noreply.github.com>
2018-02-14 12:56:57 -0500

    Displaying child components
    I couldn't get the /start folder to render the table properly, though the code looks the same.

---
+++ exercise-files/Ch04/04_02/start/src/components/SkiDayList.js
@@ -0,0 +1,28 @@
+import Terrain from 'react-icons/lib/md/terrain'
+import SnowFlake from 'react-icons/lib/ti/weather-snow'
+import Calendar from 'react-icons/lib/fa/calendar'
+import { SkiDayRow } from './SkiDayRow'
+
+export const SkiDayList = ({days}) => (
+	<table>
+		<thead>
+			<tr>
+				<th>Date</th>
+				<th>Resort</th>
+				<th>Powder</th>
+				<th>Backcountry</th>
+			</tr>
+		</thead>
+		<tbody>
+			{days.map((day, i) =>
+				<SkiDayRow 
+					key={i}
+					resort={day.resort}
+					date={day.date}
+					powder={day.powder}
+					backcountry={day.backcountry}
+					/>
+				)}
+		</tbody>
+	</table>
+)
