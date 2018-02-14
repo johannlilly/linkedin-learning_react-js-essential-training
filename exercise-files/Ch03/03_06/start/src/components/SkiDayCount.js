f97e862fca87e2f57a809a9509042ba5be2fc2cb
jlilly <johannlilly@users.noreply.github.com>
2018-02-13 21:52:44 -0500

    Creating stateless functional components

--- exercise-files/Ch03/03_06/start/src/components/SkiDayCount.js
+++ exercise-files/Ch03/03_06/start/src/components/SkiDayCount.js
@@ -1,37 +1,57 @@
 import { Component } from 'react'
 import '../stylesheets/ui.scss'
 
-export class SkiDayCount extends Component {
-	percentToDecimal(decimal) {
-		return ((decimal * 100) + '%')
-	}
-	calcGoalProgress(total, goal) {
-		return this.percentToDecimal(total/goal)
-	}
-	render() {
-		return (
-			<div className="ski-day-count">
-				<div className="total-days">
-					<span>{this.props.total}</span>
-					<span>days</span>
-				</div>
-				<div className="powder-days">
-					<span>{this.props.powder}</span>
-					<span>days</span>
-				</div>
-				<div className="backcountry-days">
-					<span>{this.props.backcountry}</span>
-					<span>days</span>
-				</div>
-				<div>
-					<span>
-						{this.calcGoalProgress(
-							this.props.total, 
-							this.props.goal
-						)}
-					</span>
-				</div>
-			</div>
-		)
-	}
+// Stateless functional components
+// a third way to create components is by using a function
+// it takes property information and returns JSX elements
+
+///	const MyComponent = (props) => (
+///		<div>{props.title}</div>
+///	)
+
+// local methods are removed and put in their own function
+
+// these methods were previously local
+// they can be used within this component or set to work with any other
+//
+///	percentToDecimal(decimal) {
+///		return ((decimal * 100) + '%')
+///	}
+///	calcGoalProgress(total, goal) {
+///		return this.percentToDecimal(total/goal)
+///	}
+const percentToDecimal = (decimal) => {
+	return ((decimal * 100) +  '%')
 }
+const calcGoalProgress = (total, goal) => {
+	return percentToDecimal(total/goal)
+}
+
+// we pass property information to this function
+// we can also destructure it
+
+// export class SkiDayCount extends Component {
+export const SkiDayCount = (props) => (
+	<div className="ski-day-count">
+		<div className="total-days">
+			<span>{props.total}</span>
+			<span>days</span>
+		</div>
+		<div className="powder-days">
+			<span>{props.powder}</span>
+			<span>days</span>
+		</div>
+		<div className="backcountry-days">
+			<span>{props.backcountry}</span>
+			<span>days</span>
+		</div>
+		<div>
+			<span>
+				{calcGoalProgress(
+					props.total, 
+					props.goal
+				)}
+			</span>
+		</div>
+	</div>
+)
