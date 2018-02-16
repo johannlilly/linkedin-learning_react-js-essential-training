0a14d4ce9631b4c04a02920a5a6ee998b10cf961
jlilly <johannlilly@users.noreply.github.com>
2018-02-16 10:46:48 -0500

    Using route parameters, links

--- exercise-files/Ch05/05_04/start/src/components/SkiDayList.js
+++ exercise-files/Ch05/05_04/start/src/components/SkiDayList.js
@@ -3,26 +3,47 @@ import SnowFlake from 'react-icons/lib/ti/weather-snow'
 import Calendar from 'react-icons/lib/fa/calendar'
 import { SkiDayRow } from './SkiDayRow'
 import { PropTypes } from 'react'
+import { Link } from 'react-router'
 
-export const SkiDayList = ({days}) => (
-	<table>
-		<thead>
-			<tr>
-				<th>Date</th>
-				<th>Resort</th>
-				<th>Powder</th>
-				<th>Backcountry</th>
-			</tr>
-		</thead>
-		<tbody>
-			{days.map((day, i) =>
-				<SkiDayRow key={i}
-						   {...day}/>	
-				)}
-		</tbody>
+// wrap JSX elements in return()
+// so we can set up variables in SkiDayList component
 
-	</table>
-)
+export const SkiDayList = ({days}) => {
+	return (
+		<div className="ski-day-list">
+		<table>
+			<thead>
+				<tr>
+					<th>Date</th>
+					<th>Resort</th>
+					<th>Powder</th>
+					<th>Backcountry</th>
+				</tr>
+				<tr>
+					<td colSpan={4}>
+						<Link to="/list-days">
+							All Days
+						</Link>
+						<Link to="/list-days/powder">
+							Powder Days
+						</Link>
+						<Link to="/list-days/backcountry">
+							Backcountry Days
+						</Link>
+					</td>
+				</tr>
+			</thead>
+			<tbody>
+				{days.map((day, i) =>
+					<SkiDayRow key={i}
+							   {...day}/>	
+					)}
+			</tbody>
+
+		</table>
+		</div>
+	)
+}
 
 SkiDayList.propTypes = {
 	days: function(props) {
