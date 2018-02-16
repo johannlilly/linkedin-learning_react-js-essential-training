3996723cf1da03ba6f78976ab942d338ff1408dc
jlilly <johannlilly@users.noreply.github.com>
2018-02-16 11:44:44 -0500

    Nesting routes

--- exercise-files/Ch05/05_05/start/src/components/index.js
+++ exercise-files/Ch05/05_05/start/src/components/index.js
@@ -1,3 +1,21 @@
+import MainMenu from './ui/MainMenu'
+
+// create page template
+// pass children variable so left component will render children dynamically
+export const Left = ({children}) =>
+	<div className="page">
+		<MainMenu className="main-menu"/>
+		{children}
+	</div>
+
+// right side nav
+export const Right = ({children}) =>
+	<div className="page">
+		{children}
+		<MainMenu className="main-menu"/>
+	</div>
+
+
 export const Whoops404 = ({ location }) =>
     <div>
         <h1>Whoops, resource not found</h1>
--- exercise-files/Ch05/05_05/start/src/routes.js
+++ exercise-files/Ch05/05_05/start/src/routes.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import { Router, Route, hashHistory } from 'react-router'
 import Home from './components/ui/Home'
-import  { Whoops404  } from './components'
+import About from '/.components/ui/About'
+import MemberList from '/.components/ui/MemberList'
+import  { Left, Right, Whoops404  } from './components'
 
+// <Route path="/" component={Left} />
+// <Route path="/" component={Right} />
 const routes = (
     <Router history={hashHistory}>
         <Route path="/" component={Home} />
+        <Route path="/" component={Right} />
+        	<Route path="about" component={About} />
+        	<Route path="members" component={MemberList} />
         <Route path="*" component={Whoops404} />
     </Router>
 )
