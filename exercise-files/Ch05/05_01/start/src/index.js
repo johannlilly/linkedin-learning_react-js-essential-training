55973ae1fd998946d74794e50383e8cb88b17db7
jlilly <johannlilly@users.noreply.github.com>
2018-02-15 23:18:10 -0500

    Incorporating the router
    Set up so that If you go to any route except home, render error.

--- exercise-files/Ch05/05_01/start/src/index.js
+++ exercise-files/Ch05/05_01/start/src/index.js
@@ -2,10 +2,20 @@ import React from 'react'
 import { render } from 'react-dom'
 import './stylesheets/ui.scss'
 import { App } from './components/App'
+import { Whoops404 } from './components/Whoops404'
+import { Router, Route, hashHistory } from 'react-router'
 
 window.React = React
 
+// Router wraps around two Route
+// the history attribute listens to and logs changes in address bar
+// Route 1 = renders app
+// Route 2 = renders anything else
+// If you go to any route except home, render error
 render(
-	<App />,
+	<Router history={hashHistory} >
+		<Route path="/" component={App} />
+		<Route path="*" component={Whoops404} />
+	</Router>,
 	document.getElementById('react-container')
 )
