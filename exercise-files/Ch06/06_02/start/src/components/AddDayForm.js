dca9532f1129051ef37072c704110f70247d8c26
jlilly <johannlilly@users.noreply.github.com>
2018-02-17 15:12:21 -0500

    Using refs in class components
    In some cases, we want to reach out to individual elements to determine what their values are. Connect components with form elements using ref=.

--- exercise-files/Ch06/06_02/start/src/components/AddDayForm.js
+++ exercise-files/Ch06/06_02/start/src/components/AddDayForm.js
@@ -1,40 +1,62 @@
 import { PropTypes, Component } from 'react'
 
 export class AddDayForm extends Component {
+
+	constructor(props) {
+		// send props
+		super(props)
+		// when we send, it needs to be accessable
+		// this is commomn in ES6 class components
+		this.submit= this.submit.bind(this)
+	}
+
+	submit(e) {
+		e.preventDefault()
+		console.log('resort', this.refs.resort.value)
+		console.log('date', this.refs.date.value)
+		console.log('powder', this.refs.powder.checked)
+		console.log('backcountry', this.refs.resort.checked)
+	}
+
 	render() {
 
 		const { resort, date, powder, backcountry } = this.props 
 
 		return (
-			<form className="add-day-form">
+			<form onSubmit={this.submit} className="add-day-form">
 
 				<label htmlFor="resort">Resort Name</label>
 				<input id="resort" 
 					   type="text" 
 					   required 
-					   defaultValue={resort}/>
+					   defaultValue={resort}
+					   ref="resort"/>
 
 				<label htmlFor="date">Date</label>
 				<input id="date" 
 					   type="date" 
 					   required 
-					   defaultValue={date}/>
+					   defaultValue={date}
+					   ref="date"/>
 
 				<div>
 					<input id="powder" 
 						   type="checkbox" 
-						   defaultChecked={powder}	/>
+						   defaultChecked={powder}	
+						   ref="powder"/>
 					<label htmlFor="powder">Powder Day</label>
 				</div>
 
 				<div>	
 					<input id="backcountry" 
 						   type="checkbox"
-						   defaultChecked={backcountry} />
+						   defaultChecked={backcountry} 
+						   ref="backcountry"/>
 					<label htmlFor="backcountry">
 						Backcountry Day
 					</label>
 				</div>
+				<button>Add Day</button>
 			</form>
 		)
 	}
