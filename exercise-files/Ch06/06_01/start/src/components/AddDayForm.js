e584608c6cb79fac41f5417f76042ee4749faa26
jlilly <johannlilly@users.noreply.github.com>
2018-02-17 13:51:52 -0500

    Creating a form component
    We want to collect values in the form. Here, we configure default values.

--- exercise-files/Ch06/06_01/start/src/components/AddDayForm.js
+++ exercise-files/Ch06/06_01/start/src/components/AddDayForm.js
@@ -1,9 +1,58 @@
-import { PropTypes } from 'react'
+import { PropTypes, Component } from 'react'
 
-export const AddDayForm = () => (
-	<h1>Add A Day</h1>
-)
+export class AddDayForm extends Component {
+	render() {
 
+		const = { resort, date, powder, backcountry } = this.props
+
+		return (
+			<form className="add-day-form">
+				<label htmlFor="resort">Resort Name</label>
+				<input 
+					id="resort"
+					type="text"
+					required
+					defaultValue={resort}
+				/>
+				<label htmlFor="date">Date</label>
+				<input
+					id="date"
+					type="date"
+					required
+					defaultValue={date}
+				/>
+				<div>
+					<label htmlFor="powder">Powder Day</label>
+					<input
+						id="powder"
+						type="checkbox"
+						required
+						defaultChecked={powder}
+					/>
+				</div>
+				<div>
+					<label htmlFor="backcountry">Backcountry Day</label>
+					<input
+						id="backcountry"
+						type="checkbox"
+						required
+						defaultChecked={backcountry}
+					/>
+				</div>
+			</form>
+		)
+	}
+}
+
+// populate data from default props
+AddDayForm.defaultProps = {
+	resort: "Kirkwood",
+	date: "2017-02-12",
+	powder: true,
+	backcountry: false
+}
+
+// collect data
 AddDayForm.propTypes = {
 	resort: PropTypes.string.isRequired,
 	date: PropTypes.string.isRequired,
