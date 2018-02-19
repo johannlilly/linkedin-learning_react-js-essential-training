7f402cba7e3c0cba6d773ec8aa942ea22fbc3d21
Johann Lilly, Certified Agile Project Manager, Certified Blockchain Expert <johannlilly@users.noreply.github.com>
2018-02-19 12:43:47 -0500

    Understanding the mounting lifecycle

--- exercise-files/Ch07/07_03/start/src/components/ui/MemberList.js
+++ exercise-files/Ch07/07_03/start/src/components/ui/MemberList.js
@@ -1,4 +1,5 @@
 import { Component } from 'react'
+// fetch connects to the randomuser.me API
 import fetch from 'isomorphic-fetch'
 import Member from './Member'
 
@@ -7,37 +8,52 @@ class MemberList extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            members: [
-            {
-                name: "Joe Wilson",
-                email: "joe.wilson@example.com",
-                thumbnail: "https://randomuser.me/api/portraits/men/53.jpg"
-            },
-            {
-                name: "Stacy Gardner",
-                email: "stacy.gardner@example.com",
-                thumbnail: "https://randomuser.me/api/portraits/women/74.jpg"
-            },
-            {
-                name: "Billy Young",
-                email: "billy.young@example.com",
-                thumbnail: "https://randomuser.me/api/portraits/men/34.jpg"
-            }
-          ]
+            members: [],
+            // set state variable
+            // trigger with fetch
+            loading: false
         }
     }
 
+    componentDidMount() {
+        this.setState({loading: true})
+        // send API route
+        fetch('http://api.randomuser.me/?nat=US&results=12')
+            // .then() is a function of fetch() that we will use to turn response into json
+            .then(response => response.json())
+            // take the json response and look for the key, 'results'
+            // this is part of the fetch API
+            .then(json => json.results)
+            // use the results to set state
+            .then(members => this.setState({
+                // reset members
+                members, // same as members: members, or members: true, but we have object-literal enhancements in ES6
+                loading: false
+            }))
+    }
+
     render() {
-    	const { members } = this.state
+        // add loading to state because we have destructured it
+    	const { members, loading } = this.state
         return (
             <div className="member-list">
                 <h1>Society Members</h1>
-                {members.map(
-                	(data, i) => 
-                		<Member key={i} 
-                				onClick={email => console.log(email)}
-                				{...data} />
-                	 )}
+
+                {(loading) ?
+                    <span>loading...</span> :
+                    <span>{members.length} members</span>
+                }
+                {/* if there are any members in state, map over them and create new members for each member in state */}
+                {(members.length) ?
+                    members.map(
+                    	(member, i) => 
+                    		<Member key={i} 
+                    				name={member.name.first + ' ' + member.name.last}
+                                    email={member.email}
+                                    thumbnail={member.picture.thumbnail} />
+                    	 ):
+                    <span>Currently 0 members</span>
+                }
             </div>
         )
     }
