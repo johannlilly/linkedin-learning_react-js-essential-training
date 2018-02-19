69e6c9f78e30daf71fd6e935b2efcb5774741703
Johann Lilly, Certified Agile Project Manager, Certified Blockchain Expert <johannlilly@users.noreply.github.com>
2018-02-18 21:30:57 -0500

    Challenge: Building the MemberList component, attempt
    members are held in state of MemberList

--- exercise-files/Ch07/07_02/start/src/components/ui/MemberList.js
+++ exercise-files/Ch07/07_02/start/src/components/ui/MemberList.js
@@ -1,9 +1,11 @@
 import { Component } from 'react'
 import fetch from 'isomorphic-fetch'
+import { Member } from './Member'
 
 class MemberList extends Component {
     constructor(props) {
         super(props)
+        // members are listed here and held in state of MemberList
         this.state = {
             members: [
             {
@@ -24,11 +26,15 @@ class MemberList extends Component {
           ]
         }
     }
-
+    // take the data in MemberList and render a Member for each member held in state
     render() {
         return (
             <div className="member-list">
             	<h1>Society Members</h1>
+                {member.map((member, i) =>
+                    <Member key={i}
+                               {...member}/>   
+                    )}
             </div>
         )    
    }     
